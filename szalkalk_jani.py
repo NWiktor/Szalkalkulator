6ff8db97dd4922a6b8da59b84703599298b90e77
NWiktor <31969508+NWiktor@users.noreply.github.com>
2020-01-11 01:39:31 +0100

    1.2 atdolgozás
    Fureszlap vastagsag_beepites
    Shortcutok univerzálissá tétele

--- szalkalk_jani.py
+++ szalkalk_jani.py
@@ -2,91 +2,129 @@
 import sys
 
 celhossz = 6000 # Elérendő szálhossz (mm)
+fureszlap_vast = 2 # Fűrészlap vastagsága (mm)
 szalak = []  # 2D tömb, benne a szálhosszakkal
 szalanyag_nev = "" ## Szálanyag elnevezése (pl.: 20x20x3 acél zártszv.)
 
 
 def help_info():
     print("\n-- INFO --")
+    print("A szálanyagok alaphosszúsága: {0} mm".format(celhossz))
+    print("A fűrészlap vastagság: {0} mm".format(fureszlap_vast))
     print("Add meg a szálhosszat és a mennyiséget vesszővel elválasztva (pl.: 1300,4)")
     print("Az adatbevitel befejezéséhez és az összegzéshez írj be egy 'x' karaktert.")
     print("A szálhossz alapértékének módosításához írj be egy 'm' karaktert.")
+    print("A fűrészlap vastagság módosításához írj be egy 'f' karaktert.")
     print("A help ismételt előhívásához nyomd meg az 'i' karaktert.")
     print("A kilépéshez nyomd meg az 'e' karaktert.")
+
+
+def mod_fureszlap(): # Fűrészlap módosítása
+    global fureszlap_vast
+
+    try: #Input konvertálása integerré
+        fureszlap_vast = int(key_input("\nA fűrészlap új vastagsága: "))
+
+    except:
+        print("Hibás adatbevitel! A fűrészlap vastagsága nem változott: {0} mm".format(fureszlap_vast))
+        return
+
+    print("A fűrészlap vastagság sikeresen megváltoztatva!")
+
+
+def key_input(string):
+    ki = input(string)
+
+    if ki == "e":
+        raise Escape()
+    elif ki == "i":
+        help_info()
+    elif ki == "m":
+        mod_celhossz()
+    elif ki == "f":
+        mod_fureszlap()
+    elif ki == "x":
+        tulhossz_torles()
+        if szalak == []:
+            print("Nem adtál hozzá vágandó szálat!")
+        else:
+            kalk()
+            inputs()
+    else:
+        return ki
 
 
 def inputs():
     global szalanyag_nev
     input_list = []
-    szalanyag_nev = input("\nAdd meg a vágandó szál nevét: ")
 
-    if szalanyag_nev == "e": ## Ha a beírt billentyű 'e', akkor kilép.
-        raise Escape()
-
-    else:
-        print("Szálanyag: {0}".format(szalanyag_nev))
-
-    while True:
+    while True: # Száltípus elnevezése
         try:
-            input_list = input("\nAdd meg a vágandó szál hosszát és a mennyiségét: ").split(",")
+            szalanyag_nev = key_input("\nAdd meg a vágandó szál nevét: ")
+            if szalanyag_nev == None:
+                continue
 
-            if input_list[0] == "x":
-                print("Adatbevitel vége!")
-                break
-
-            elif input_list[0] == "e":
-                raise Escape()
-
-            elif input_list[0] == "m":
-                mod_celhossz()
-
-            elif input_list[0] == "i":
-                help_info()
-
-            else:
-                hossz = int(input_list[0])
-
-                try:
-                    menny = int(input_list[1])
-
-                except IndexError:
-                    menny = 1
-                    print("Hiányos adatbevitel! A mennyiség alapértéke: 1!")
-
-                add_szal(hossz, menny)
+            break
 
         except ValueError:
-            print(" Érték hiba! Kérlek számértéket adj meg!")
+            print("Nem megfelelő elnevezés!")
+
+    print("Szálanyag: {0}".format(szalanyag_nev))
+
+    while True: # Vágandó szálak hozzáadása
+        try:
+            data = key_input("\nAdd meg a vágandó szál hosszát és a mennyiségét: ")
+            if data == None:
+                continue
+            else:
+                input_list = data.split(",")
+
+            hossz = int(input_list[0])
+
+            try:
+                menny = int(input_list[1])
+
+            except IndexError:
+                menny = 1
+                print("Hiányos adatbevitel! A mennyiség beállítva: 1 db!")
+
+            except ValueError:
+                print("Mennyiségi érték hiba! Kérlek számértéket adj meg!")
+
+            add_szal(hossz, menny)
+
+        except ValueError:
+            print("Szálhossz érték hiba! Kérlek számértéket adj meg!")
+
+
+def tulhossz_torles(): ## Célhossznál nagyobb elemek utúlagos törlése - ha a hosszú elem korábban lett a listához adva
+    if szalak == []: return
+    torolt_hossz = []
+
+    szalak.sort(reverse=True) # Szalak forditott sorrendbe állitasa
+    while szalak != []:
+        if szalak[0] > celhossz: # Ha az első elem hosszabb mint a célhossz
+            torolt_hossz.append(szalak[0]) # A törlendő elemet hozzáadjuk a listához
+            szalak.pop(0) # Az első elemet kivesszük a listából
+        else:
+            break
+
+    if torolt_hossz != []:
+        print("A célhossznál hosszabb elemek {0} törölve a listából!".format(torolt_hossz))
 
 
 def mod_celhossz(): # Célhossz módosítása
     global celhossz
-    celh = 0
-    torolni_hosszu = []
-    torolt_hossz = []
 
     try: #Input konvertálása integerré
-        celh = int(input("\nA szálhossz új alapértéke: "))
-        celhossz = celh
+        celhossz = int(key_input("\nA szálhossz új alapértéke: "))
 
     except:
-        print("Hibás adatbevitel! A szálhossz értéke nem változott: {0}".format(celhossz))
+        print("Hibás adatbevitel! A szálhossz értéke nem változott: {0} mm".format(celhossz))
         return
 
-    ## Célhossznál nagyobb elemek utúlagos törlése - ha a hosszú elem korábban lett a listához adva
-    for s in range(0, len(szalak)):
-        if szalak[s] > celhossz:
-            torolni_hosszu.append(s) # A törlendő indexet hozzáadjuk a listához
-            torolt_hossz.append(szalak[s]) # A törlendő elemet hozzáadjuk a listához
-        else:
-            continue
-
-    torolni_hosszu.sort(reverse=True)
-    for st in range(0, len(torolni_hosszu)): # a túl hosszú darabok törlése a tömbből
-        szalak.pop(torolni_hosszu[st])
-
-    print("Célhossznál hosszabb elemek ({0}) törölve a listából!".format(torolt_hossz))
-    print("\nA szálhossz alapérték sikeresen megváltoztatva!")
+    print("A szálhossz alapérték sikeresen megváltoztatva!")
+    tulhossz_torles()
 
 
 def add_szal(szalhossz, menny): # szálanyag hozzáadása a tömbhöz
@@ -94,9 +132,7 @@ def add_szal(szalhossz, menny): # szálanyag hozzáadása a tömbhöz
         print("Hibás adatbevitel: túl nagy szálhossz!")
 
     else:
-        for i in range (0, menny):
-            szalak.append(szalhossz)
-
+        for i in range (0, menny): szalak.append(szalhossz)
         print("{0} db, {1} mm szálanyag hozzáadva a listához!".format(menny, szalhossz))
 
 
@@ -106,27 +142,24 @@ def kalk():
     hulladek_hossz = 0
     hulladek_szazalek = 0
     eredmeny = [] # eredmény gyűjtő tömb
-    hulladek = []
+    hulladek = [] #hulladek gyüjtő tömb
 
     while szalak != []: # Amíg a szálak tömb nem nulla, fut a számítás
-        akt_szal = "|" # akutális szál összetétele / string
+        akt_szal = "|" # akutális szál összetétele / string formátumban
         akt_hossz = 0 # aktuális szál szálhossza
         maradek = celhossz ## aktuális szál maradéka
         torolni = []
 
         for i in range(0, len(szalak)): # Végigmegyek a szálak tömb minden elemén
-            if szalak[i] <= maradek: # Ha az aktuális száldarab rövidebb, mint a maradék
+            if szalak[i] <= maradek: # Ha az aktuális száldarab rövidebb v egyenlő, mint a maradék
                 akt_hossz += szalak[i]  # Aktuális szálhosszhoz hozzáadom az elemet
                 maradek -= szalak[i] # Maradékból kivonom az elemet
                 akt_szal += "| {0:>4} ".format(szalak[i]) # A szál összetételhez hozzáadom az elemet
-                torolni.append(i) # torlendp elemek indexéhez hozzáadni az aktuálisat
+                torolni.append(i) # torlendő elemek index listájához hozzáadni az aktuálisat
 
             else: # Ha az aktuális száldarab hosszabb, mint a maradék
-                if maradek < szalak[len(szalak)-1]: # Ha a maradék kisebb, mint az utolsó (legrövidebb elem), akkor kilépek
-                    break
-
-                else:
-                    continue
+                if maradek < szalak[-1]: # Ha a maradék kisebb, mint az utolsó (legrövidebb elem), akkor kilépek
+                    break # For ciklus megtörése, kilépés a while ciklusba
 
         # Ha kész az iteráció összesítem a szálat
         akt_szal += "|| --> {0} mm".format(akt_hossz)
@@ -136,10 +169,7 @@ def kalk():
             hulladek.append(maradek)
             hulladek_hossz += maradek
 
-        else:
-            pass
-
-        torolni.sort(reverse=True) # torlendo indexek megforditasa
+        torolni.sort(reverse=True) # torlendo indexek csökkenő sorrendbe állítása ( ez kell? )
 
         for k in range(0, len(torolni)): #felhasznált darabok törlése a tömbből
             szalak.pop(torolni[k])
@@ -161,14 +191,12 @@ def kalk():
     print("\n-- ÖSSZEGZÉS -- {0} --".format(szalanyag_nev))
     print("Szükséges szálmennyiség: {0} db ({1} mm)".format(len(eredmeny), celhossz))
     print("Hulladék mennyisége:     {0:.2f} % ({1} mm)".format(hulladek_szazalek, hulladek_hossz))
-
-    # Hulladek darabok
-    print(hulladek)
+    print("Hulladékok: " + str(hulladek)) # Hulladek darabok
 
 
 def credits():
-    print("\nSzalkalkulator V1.0.1")
-    print("Wetzl Viktor - 2020-01-08 - (C) Minden jog fenntartva!")
+    print("\nSzalkalkulátor V1.0.1")
+    print("Wetzl Viktor - 2020-01-11 - (C) Minden jog fenntartva!")
 
 
 class Escape(Exception):
@@ -182,10 +210,9 @@ class Escape(Exception):
 print("\nHello Jani!")
 help_info() ## Alap parancsok kiírása
 
-try:
-    while True:
-        inputs()
-        kalk()
+try: # Amíg ki nem lépek Escape exceptionnel, addig ismétli a kalkulációt/kiértékelést az inputs fv.-en belüli loop miatt
+    # while True:
+    inputs()
 
 except Escape as e:
     print(e.value)
